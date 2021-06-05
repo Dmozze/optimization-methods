fa15eb22a04e255d1e2cbcdcadb4b18cdf284222
lejabque <vorkdenis1@gmail.com>
2021-06-05 15:28:37 +0300

    first markvard version (fix files)

---
+++ fourth-lab-markvard.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "fourth-lab/utils.h"
+
+#include "fourth-lab/MarkvardMethods.h"
+//
+static void RunMarkvardMethod(IMarkvardMethod& method) {
+    auto v = method.Minimize();
+    std::cout << "Method name: " << typeid(method).name() << "\n"
+              << "Found answer:  " << v;
+    std::cout << " f(x) = " << method.GetFunc().FuncApplier(v) << "\n";
+    std::cout << "Iterations: " << method.IterCounter << "\n"
+              << std::endl;
+}
+
+void RunAllMethods(FunctionData const& func, const Vector& start) {
+    std::cout << "Start: ";
+    std::cout << start << "\n";
+
+    TMarkvardFirst firstMarkvard(func, start);
+    RunMarkvardMethod(firstMarkvard);
+}
+
+using namespace Tests;
+
+int main() {
+    // TestSimpleQuadratic({-5, -2});
+    TestPseudoNonQuadratic({0.1, 0});
+    //TestPseudoNonQuadratic({-100, 200});
+    //    TestF1({4, 1});
+    // TestF2({-1.2, 1});
+    // TestF2({0, 0});
+    // TestF2({5, 5});
+    //    TestF3({0, 0});
+    //    TestF3({-5, -5});
+    //    TestF3({2, 3});
+
+    //  TestF4({1, 1, 1, 1});
+    //  TestF4({-1, 0, -1, 0});
+    // TestF4({1, 0, 0, 0});
+    //    TestF5({0, 0});
+    //    TestF5({5, 5});
+    //    TestF5({-1, -1});
+}
---
+++ fourth-lab/MarkvardMethods.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "utils.h"
+
+#include <GaussSolver.h>
+#include <search-metods.h>
+
+#include <utility>
+#include <vector>
+#include <functional>
+#include <algebra/Vector.h>
+#include <algebra/Matrix.h>
+#include <ConjugateSolver.h>
+
+class IMarkvardMethod {
+    static constexpr bool GRAPHS = true;
+
+public:
+    explicit IMarkvardMethod(FunctionData f, Vector startX, long double tau)
+        : Func(std::move(f))
+        , CurrentX(std::move(startX))
+        , Tau(tau) {
+    }
+
+    virtual Vector Minimize() = 0;
+
+    FunctionData GetFunc() const {
+        return Func;
+    }
+
+    uint64_t IterCounter = 0;
+
+protected:
+    FunctionData Func;
+    Vector CurrentX;
+    Vector P;
+    long double Tau;
+};
+
+class TMarkvardFirst : public IMarkvardMethod {
+public:
+    explicit TMarkvardFirst(FunctionData f, Vector startX)
+        : IMarkvardMethod(std::move(f), std::move(startX), 100) {
+    }
+
+    Vector Minimize() override {
+        while (true) {
+            while (true) {
+                auto H = Func.HessianApplier(CurrentX);
+                H = OneMatrix(CurrentX.size()) * Tau + H;
+                auto g = Func.GradApplier(CurrentX) * -1;
+                if (P.size() == 0) {
+                    P = Vector(CurrentX.size());
+                }
+                ConjugateSolve(H, g, EPS, P);
+                auto y = CurrentX + P;
+                PrevX = CurrentX;
+                if (Func.FuncApplier(y) > Func.FuncApplier(CurrentX)) {
+                    Tau /= 0.5;
+                    continue;
+                }
+                CurrentX = y;
+                Tau *= 0.5;
+                break;
+            }
+            if (P.Norm() < EPS) {
+                break;
+            }
+        }
+        return CurrentX;
+    }
+
+private:
+    Vector PrevX;
+};
